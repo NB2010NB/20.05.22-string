02cfa47a496f51311942029d01814e3d7df991c4
NB2010NB <natanbalai1@gmail.com>
2022-05-26 09:45:45 +0300

    naster

---
+++ main.js
@@ -0,0 +1,28 @@
+//*******112******** */
+// function printTav(str) {
+//     for (var i = 0; i < str.length; i++) {
+//         console.log(str[i]);
+//     }
+// } printTav("hello world")
+
+//*******113******* */
+
+// function tavAndNumber(jbl) {
+//     for (var i = 0; i < jbl.length; i++) {
+//         console.log(jbl[i]+i);
+//     }
+// } tavAndNumber("natanbelay")
+
+
+//********114****** */
+function number2(cupl) {
+    for (var i = 0; i < cupl.length; i++) {
+        if (cupl.length % 2 == 0) {
+            cupl[i]= 2
+
+        }
+        
+        console.log(cupl);
+    }
+    
+} number2("whatshap")
